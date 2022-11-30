4dc0177e2192223b57082e6a4959f6acc1a38088
Caleb Stromberg <calebstromberg@gmail.com>
2022-11-30 15:30:49 -0800

    Add css reset and hot reloading

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
+import "./stylesheets/meyer-reset.css";
+import "./stylesheets/style.css";
+
 console.log("Hello world");
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,30 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     entry: "./src/index.js",
+    devtool: "inline-source-map",
     mode: "development",
+    devServer: {
+        static: "./src"
+    },
     output: {
         filename: "[name].bundle.js",
         path: path.resolve(__dirname, "dist"),
+        clean: true,
     },
-}
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Here's the todo list!",
+            template: "./src/index.html",
+        })
+    ],
+};
